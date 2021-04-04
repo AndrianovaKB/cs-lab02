fa3365422ed01f21de33873af5e2844a2922baa5
Bob (Andriyanova KB) <krisandry2002@gmail.com>
2021-04-04 20:21:31 +0300

    min

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
+#include <conio.h>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b, max;
+    int a, b, max, min;
     cin >> a >> b;
     max=0;
+    min=0;
     if(a>b)
-        cout << "max=a" << '\n';
+    {
+        max=a;
+        min=b;
+    }
     else if(a==b)
-        cout << "max=0" << '\n';
+        cout << "a=b" <<'\n';
     else
-        cout << "max=b" << '\n';
+    {
+        max=b;
+        min=a;
+    }
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
+         cout << "min=" << min << '\n'
+              << "max=" << max << '\n';
     return 0;
 }
